e8f1feda6b6af771ca973045d3d3c17cc9640c42
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-10-28 14:13:46 +0000

    Move error handling into function

--- Translator/src/SchemaTranslator/Translator.java
+++ Translator/src/SchemaTranslator/Translator.java
@@ -20,55 +20,55 @@ public class Translator {
 		System.out.println("Database dump starting...");
 		String neo4jDBPath = "D:\\Program Files\\Neo4j\\Neo4j CE 3.2.6\\databases\\graph.db";
 		String neo4jDumpFilePath = "D:\\Program Files\\Neo4j\\Neo4j CE 3.2.6\\databases\\dump.txt";
+		Translator.dumpGraphDatabase(neo4jDBPath, neo4jDumpFilePath);
+		System.out.println("Database dump complete");
+		
+		System.out.println("Dump file cleaning started...");
+		Translator.cleanDumpFile(Paths.get(neo4jDumpFilePath));
+		System.out.println("Dump file cleaning complete");
+		
+		System.out.println("Testing Postgres connection...");
+		Translator.connectPostgres();
+		System.out.println("Testing Postgres complete");
+	}
+	
+	public static void dumpGraphDatabase(String neo4jDBPath, String neo4jDumpFilePath) {		
 		try {
-			Translator.dumpGraphDatabase(neo4jDBPath, neo4jDumpFilePath);
+			String[] neo4jDumpArgs = {"-path", neo4jDBPath, "-c", "dump"};
+			PrintStream neo4jDumpOut = new PrintStream(neo4jDumpFilePath);
+			System.setOut(neo4jDumpOut);
+			StartClient.main(neo4jDumpArgs);
+			System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+			neo4jDumpOut.close();
 		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-		System.out.println("Database dump complete");
-		
-		System.out.println("Dump file cleaning started...");
+	}
+	
+	public static void cleanDumpFile(Path neo4jDumpFilePath) {
 		try {
-			Translator.cleanDumpFile(Paths.get(neo4jDumpFilePath));
+			List<String> dumpFileContents = Files.readAllLines(neo4jDumpFilePath);
+			dumpFileContents.remove(0);
+			dumpFileContents.remove(0);
+			dumpFileContents.remove(0);
+			dumpFileContents.remove(0);
+			dumpFileContents.remove(dumpFileContents.size() - 1);
+			dumpFileContents.remove(dumpFileContents.size() - 1);
+			Files.write(neo4jDumpFilePath, dumpFileContents);
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-		System.out.println("Dump file cleaning complete");
-		
-		System.out.println("Testing Postgres connection...");
+	}
+	
+	public static void connectPostgres() {
 		try {
-			Translator.connectPostgres();
+			Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "admin");
+			connection.close();
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
-		System.out.println("Testing Postgres complete");
-	}
-	
-	public static void dumpGraphDatabase(String neo4jDBPath, String neo4jDumpFilePath) throws FileNotFoundException {		
-		String[] neo4jDumpArgs = {"-path", neo4jDBPath, "-c", "dump"};
-		PrintStream neo4jDumpOut = new PrintStream(neo4jDumpFilePath);
-		System.setOut(neo4jDumpOut);
-		StartClient.main(neo4jDumpArgs);
-		System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
-		neo4jDumpOut.close();
-	}
-	
-	public static void cleanDumpFile(Path neo4jDumpFilePath) throws IOException {
-		List<String> dumpFileContents = Files.readAllLines(neo4jDumpFilePath);
-		dumpFileContents.remove(0);
-		dumpFileContents.remove(0);
-		dumpFileContents.remove(0);
-		dumpFileContents.remove(0);
-		dumpFileContents.remove(dumpFileContents.size() - 1);
-		dumpFileContents.remove(dumpFileContents.size() - 1);
-		Files.write(neo4jDumpFilePath, dumpFileContents);
-	}
-	
-	public static void connectPostgres() throws SQLException {
-		Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "admin");
-		connection.close();
 	}
 }
