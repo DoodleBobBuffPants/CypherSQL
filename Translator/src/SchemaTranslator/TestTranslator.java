53c1d6fd7f49e577936cc29342571c20acdf532d
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-10-28 15:00:09 +0000

    Add tests for dump file

---
+++ Translator/src/SchemaTranslator/TestTranslator.java
@@ -0,0 +1,38 @@
+package SchemaTranslator;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import org.junit.Test;
+
+public class TestTranslator {
+	@Test
+	public void testDumpGraphDatabase() {
+		String neo4jDBPath = "D:\\Program Files\\Neo4j\\Neo4j CE 3.2.6\\databases\\graph.db";
+		String neo4jDumpFilePath = "D:\\Program Files\\Neo4j\\Neo4j CE 3.2.6\\databases\\dump.txt";
+		
+		Translator.dumpGraphDatabase(neo4jDBPath, neo4jDumpFilePath);
+		
+		File neo4jDumpFile = new File(neo4jDumpFilePath);
+		assertTrue(neo4jDumpFile.exists());
+	}
+
+	@Test
+	public void testCleanDumpFile() {
+		String neo4jDumpFilePath = "D:\\Program Files\\Neo4j\\Neo4j CE 3.2.6\\databases\\dump.txt";
+		
+		Translator.cleanDumpFile(Paths.get(neo4jDumpFilePath));
+		
+		File neo4jDumpFile = new File(neo4jDumpFilePath);
+		assertTrue(neo4jDumpFile.exists());
+	}
+	
+	@Test
+	public void testConnectPostgres() {
+		Translator.connectPostgres();
+		
+		assertTrue(true);
+	}
+}
--- Translator/src/SchemaTranslator/Translator.java
+++ Translator/src/SchemaTranslator/Translator.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -16,22 +15,6 @@ import java.util.List;
 import org.neo4j.shell.StartClient;
 
 public class Translator {
-	public static void main(String[] args) {
-		System.out.println("Database dump starting...");
-		String neo4jDBPath = "D:\\Program Files\\Neo4j\\Neo4j CE 3.2.6\\databases\\graph.db";
-		String neo4jDumpFilePath = "D:\\Program Files\\Neo4j\\Neo4j CE 3.2.6\\databases\\dump.txt";
-		Translator.dumpGraphDatabase(neo4jDBPath, neo4jDumpFilePath);
-		System.out.println("Database dump complete");
-		
-		System.out.println("Dump file cleaning started...");
-		Translator.cleanDumpFile(Paths.get(neo4jDumpFilePath));
-		System.out.println("Dump file cleaning complete");
-		
-		System.out.println("Testing Postgres connection...");
-		Translator.connectPostgres();
-		System.out.println("Testing Postgres complete");
-	}
-	
 	public static void dumpGraphDatabase(String neo4jDBPath, String neo4jDumpFilePath) {		
 		try {
 			String[] neo4jDumpArgs = {"-path", neo4jDBPath, "-c", "dump"};
