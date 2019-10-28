4aa586e9cb76366201f5b83b0b4c73282482173e
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-10-28 15:52:47 +0000

    Implementing passing a graph in front end API

--- Translator/src/FrontEndAPI/API.java
+++ Translator/src/FrontEndAPI/API.java
@@ -1,7 +1,43 @@
 package FrontEndAPI;
 
+import SchemaTranslator.Translator;
+
 public class API {
 	public static void main(String[] args) {
-		System.out.println("Good morning world");
+		if (args.length != 2) {
+			System.out.println("Must contain two arguments: <option> <argument>");
+			System.out.println("Options are --graphdb --schema --query");
+		} else {
+			String option = args[0];
+			String argument = args[1];
+			
+			switch (option) {
+				case "--graphdb":
+					handleGraphDB(argument);
+					break;
+				case "--schema":
+					handleSchema(argument);
+					break;
+				case "--query":
+					handleQuery(argument);
+					break;
+				default:
+					System.out.println("Invalid option");
+			}
+		}
+	}
+	
+	private static void handleGraphDB(String graphDBPath) {
+		String dumpFilePath = graphDBPath + "dump.txt";
+		Translator.dumpGraphDatabase(graphDBPath, dumpFilePath);
+		Translator.cleanDumpFile(dumpFilePath);
+	}
+	
+	private static void handleSchema(String schema) {
+		System.out.println("Not yet implemented");
+	}
+	
+	private static void handleQuery(String query) {
+		System.out.println("Not yet implemented");
 	}
 }
