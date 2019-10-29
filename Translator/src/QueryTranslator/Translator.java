4978bf85be685c1093a0c3c99367d217412566f1
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-10-29 15:59:21 +0000

    Parse the dump file

--- Translator/src/QueryTranslator/Translator.java
+++ Translator/src/QueryTranslator/Translator.java
@@ -1,7 +1,32 @@
 package QueryTranslator;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.antlr.v4.gui.Trees;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import antlr4.CypherLexer;
+import antlr4.CypherParser;
+
 public class Translator {
-	public static void main(String[] args) {
-		System.out.println("Good morning world");
+	public static void main(String[] args) throws IOException {
+		String neo4jDumpFilePath = "D:\\Program Files\\Neo4j\\Neo4j CE 3.2.6\\databases\\graph.dbdump.txt";
+		List<String> dumpFileContents = Files.readAllLines(Paths.get(neo4jDumpFilePath));
+		String firstCreate = dumpFileContents.get(0);
+		
+		CharStream stringStream = CharStreams.fromString(firstCreate);
+		CypherLexer lexer = new CypherLexer(stringStream);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		
+		tokens.fill();
+		System.out.println(tokens.getText());
+		
+		CypherParser parser = new CypherParser(tokens);
+		Trees.inspect(parser.oC_Cypher(), parser);
 	}
 }
