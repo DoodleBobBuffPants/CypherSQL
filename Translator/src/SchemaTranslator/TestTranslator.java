8bd1c3e977d32f1a5ac1f082be2d84a81e8d5e26
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-10-30 18:03:14 +0000

    Add test for creating AST

--- Translator/src/SchemaTranslator/TestTranslator.java
+++ Translator/src/SchemaTranslator/TestTranslator.java
@@ -3,6 +3,7 @@ package SchemaTranslator;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 import org.junit.Test;
 
@@ -16,6 +17,16 @@ public class TestTranslator {
 		
 		File neo4jDumpFile = new File(neo4jDBPath + "dump.txt");
 		assertTrue(neo4jDumpFile.exists());
+	}
+	
+	@Test
+	public void testCreateAST() {
+		String neo4jDumpFilePath = "D:\\Program Files\\Neo4j\\Neo4j CE 3.2.6\\databases\\graph.dbdump.txt";
+		Translator schemaTranslator = new Translator(Paths.get(neo4jDumpFilePath));
+		
+		schemaTranslator.createAST();
+		
+		assertTrue(true);
 	}
 	
 	@Test
