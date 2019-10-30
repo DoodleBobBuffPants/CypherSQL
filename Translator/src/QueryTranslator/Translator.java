9685001f141ae8c1d6d271e7f82099f5c560713e
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-10-30 17:48:45 +0000

    Construct AST in the schema package

--- Translator/src/QueryTranslator/Translator.java
+++ Translator/src/QueryTranslator/Translator.java
@@ -1,65 +1,7 @@
 package QueryTranslator;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-
-import org.antlr.v4.gui.Trees;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
-
-import SchemaAST.Create;
-import SchemaAST.CreateEdge;
-import SchemaAST.CreateNode;
-import SchemaTranslator.CreateListener;
-import antlr4.CypherLexer;
-import antlr4.CypherParser;
-
 public class Translator {
-	public static void main(String[] args) throws IOException {
-		String neo4jDumpFilePath = "D:\\Program Files\\Neo4j\\Neo4j CE 3.2.6\\databases\\graph.dbdump.txt";
-		List<String> dumpFileContents = Files.readAllLines(Paths.get(neo4jDumpFilePath));
-		String firstCreateStatement = dumpFileContents.get(0);
-		
-		CharStream fcsStream = CharStreams.fromString(firstCreateStatement);
-		CypherLexer inputLexer = new CypherLexer(fcsStream);
-		CommonTokenStream tokens = new CommonTokenStream(inputLexer);
-		CypherParser inputParser = new CypherParser(tokens);
-		
-		ParseTree parseTree = inputParser.oC_Cypher();
-		Trees.inspect(parseTree, inputParser);
-		
-		ParseTreeWalker treeWalker = new ParseTreeWalker();
-		CreateListener createListener = new CreateListener();
-		treeWalker.walk(createListener, parseTree);
-		
-		createListener.getCreateStack().forEach(n -> {printCreate(n); System.out.println();});
-		createListener.getLabelTables().forEach((k, v) -> System.out.println(k + ": " + v));
-		createListener.getTypeTables().forEach((k, v) -> System.out.println(k + ": " + v));
-	}
-	
-	public static void printCreate(Create create) {
-		if (create instanceof CreateNode) {
-			printCreateNode((CreateNode) create);
-		} else {
-			printCreateEdge((CreateEdge) create);
-		}
-	}
-	
-	public static void printCreateNode(CreateNode createNode) {
-		System.out.println("id: " + createNode.getId());
-		System.out.println("labels: " + createNode.getLabelList());
-		createNode.getColumnValueMap().forEach((k, v) -> System.out.println(k + ": " + v));
-	}
-	
-	public static void printCreateEdge(CreateEdge createEdge) {
-		System.out.println("source: " + createEdge.getSourceID());
-		System.out.println("target: " + createEdge.getTargetID());
-		System.out.println("type: " + createEdge.getType());
-		createEdge.getColumnValueMap().forEach((k, v) -> System.out.println(k + ": " + v));
+	public static void main(String[] args) {
+		System.out.println("Good morning world");
 	}
 }
--- Translator/src/SchemaTranslator/Translator.java
+++ Translator/src/SchemaTranslator/Translator.java
@@ -12,7 +12,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.neo4j.shell.StartClient;
+
+import SchemaAST.Create;
+import SchemaAST.CreateEdge;
+import SchemaAST.CreateNode;
+import antlr4.CypherLexer;
+import antlr4.CypherParser;
 
 public class Translator {
 	String neo4jDBPath;
@@ -32,6 +43,7 @@ public class Translator {
 			System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
 			neo4jDumpOut.close();
 			cleanDumpFile();
+			createAST();
 		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
@@ -52,6 +64,53 @@ public class Translator {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
+	}
+	
+	private void createAST() {
+		try {
+			List<String> dumpFileContents = Files.readAllLines(Paths.get(neo4jDumpFilePath));
+			
+			ParseTreeWalker treeWalker = new ParseTreeWalker();
+			CreateListener createListener = new CreateListener();
+			
+			for (String statement: dumpFileContents) {
+				CharStream statementStream = CharStreams.fromString(statement);
+				CypherLexer inputLexer = new CypherLexer(statementStream);
+				CommonTokenStream tokens = new CommonTokenStream(inputLexer);
+				CypherParser inputParser = new CypherParser(tokens);
+				
+				ParseTree parseTree = inputParser.oC_Cypher();
+				treeWalker.walk(createListener, parseTree);
+			}
+			
+			createListener.getCreateStack().forEach(e -> {printCreate(e); System.out.println();});
+			createListener.getLabelTables().forEach((k, v) -> System.out.println(k + ": " + v));
+			createListener.getTypeTables().forEach((k, v) -> System.out.println(k + ": " + v));
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	private void printCreate(Create create) {
+		if (create instanceof CreateNode) {
+			printCreateNode((CreateNode) create);
+		} else {
+			printCreateEdge((CreateEdge) create);
+		}
+	}
+	
+	private void printCreateNode(CreateNode createNode) {
+		System.out.println("id: " + createNode.getId());
+		System.out.println("labels: " + createNode.getLabelList());
+		createNode.getColumnValueMap().forEach((k, v) -> System.out.println(k + ": " + v));
+	}
+	
+	private void printCreateEdge(CreateEdge createEdge) {
+		System.out.println("source: " + createEdge.getSourceID());
+		System.out.println("target: " + createEdge.getTargetID());
+		System.out.println("type: " + createEdge.getType());
+		createEdge.getColumnValueMap().forEach((k, v) -> System.out.println(k + ": " + v));
 	}
 	
 	public static void connectPostgres() {
