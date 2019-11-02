82cf70459818f1a2f03121a39f1d7a341ee49d5b
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-11-02 19:40:28 +0000

    Add empty test for Formatter

---
+++ Translator/src/ResultFormatter/TestFormatter.java
@@ -0,0 +1,12 @@
+package ResultFormatter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestFormatter {
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
