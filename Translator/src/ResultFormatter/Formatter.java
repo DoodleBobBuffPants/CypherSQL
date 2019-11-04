a1a290610d84c3f25a2c36be7e6853b87312bcd2
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-11-04 15:51:25 +0000

    Refactor formatter

--- Translator/src/ResultFormatter/Formatter.java
+++ Translator/src/ResultFormatter/Formatter.java
@@ -16,19 +16,31 @@ import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 public class Formatter {
-	private Set<Map<String, String>> neo4jResult = new HashSet<Map<String, String>>();
-	private Set<Map<String, String>> postgresResult = new HashSet<Map<String, String>>();
+	private Set<Map<String, String>> neo4jResult;
+	private Set<Map<String, String>> postgresResult;
 	
 	public static void main(String[] args) {
 		Formatter formatter = new Formatter();
-		formatter.getNeo4JResult("D:\\Program Files\\Neo4j\\Neo4j CE 3.2.6\\databases\\graph.db");
-		formatter.getPostgresResult();
+		formatter.initialiseResultSets();
+		String neo4jQuery = "MATCH (n) RETURN labels(n) AS label, count(*) AS count";
+		formatter.getNeo4JResult("D:\\Program Files\\Neo4j\\Neo4j CE 3.2.6\\databases\\graph.db", neo4jQuery);
+		String postgresQuery = "SELECT relname AS label, n_live_tup AS count "
+				+ "FROM pg_stat_user_tables t "
+				+ "INNER JOIN information_schema.columns c ON c.table_name = t.relname "
+				+ "WHERE c.column_name = 'nodeid' "
+				+ "AND t.relname <> 'nodes'";
+		formatter.getPostgresResult("graph", postgresQuery);
 		System.out.println(formatter.compare());
 	}
 	
-	public void getNeo4JResult(String neo4jDBPath) {
+	public void initialiseResultSets() {
+		neo4jResult =  new HashSet<Map<String, String>>();
+		postgresResult =  new HashSet<Map<String, String>>();
+	}
+	
+	public void getNeo4JResult(String neo4jDBPath, String query) {
 		GraphDatabaseService neo4jConnection = new GraphDatabaseFactory().newEmbeddedDatabase(new File(neo4jDBPath));
-		Result result = neo4jConnection.execute("MATCH (n) RETURN labels(n) AS label, count(*) AS count");
+		Result result = neo4jConnection.execute(query);
 		while (result.hasNext()) {
 			Map<String, Object> row = result.next();
 			Map<String, String> newElement = new HashMap<String, String>();
@@ -38,15 +50,11 @@ public class Formatter {
 		neo4jConnection.shutdown();
 	}
 	
-	public void getPostgresResult() {
+	public void getPostgresResult(String dbName, String query) {
 		try {
-			Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/graph", "postgres", "admin");
+			Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbName, "postgres", "admin");
 			Statement statement = connection.createStatement();
-			ResultSet result = statement.executeQuery("SELECT relname AS label, n_live_tup AS count "
-					+ "FROM pg_stat_user_tables t "
-					+ "INNER JOIN information_schema.columns c ON c.table_name = t.relname "
-					+ "WHERE c.column_name = 'nodeid' "
-					+ "AND t.relname <> 'nodes'");
+			ResultSet result = statement.executeQuery(query);
 			while(result.next()) {
 				Map<String, String> newElement = new HashMap<String, String>();
 				for (int i = 1; i <= result.getMetaData().getColumnCount(); i++) {
