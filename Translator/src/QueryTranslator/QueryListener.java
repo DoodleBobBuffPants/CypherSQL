2e65fd12e2a7fc2673481f56c8a0b6a3b496e49c
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-11-06 22:28:16 +0000

    Finish template for Cypher AST

--- Translator/src/QueryTranslator/QueryListener.java
+++ Translator/src/QueryTranslator/QueryListener.java
@@ -1,35 +1,30 @@
 package QueryTranslator;
 
 import QueryAST.Match;
+import QueryAST.NodePattern;
 import QueryAST.Query;
 import QueryAST.Return;
-import QueryAST.SinglePartQuery;
 import antlr4.CypherParser;
 
 public class QueryListener extends antlr4.CypherBaseListener {
 	private Query query;
-	private SinglePartQuery singlePartQuery;
 	private Match matchClause;
 	private Return returnClause;
+	private NodePattern nodePattern;
 	
 	public Query getQuery() {
 		return query;
 	}
 	
 	@Override
+	public void enterOC_Query(CypherParser.OC_QueryContext ctx) {
+		query = new Query();
+	}
+	
+	@Override
 	public void exitOC_Query(CypherParser.OC_QueryContext ctx) {
-		query = singlePartQuery;
-	}
-	
-	@Override
-	public void enterOC_SinglePartQuery(CypherParser.OC_SinglePartQueryContext ctx) {
-		singlePartQuery = new SinglePartQuery();
-	}
-	
-	@Override
-	public void exitOC_SinglePartQuery(CypherParser.OC_SinglePartQueryContext ctx) {
-		singlePartQuery.setMatchClause(matchClause);
-		singlePartQuery.setReturnClause(returnClause);
+		query.setMatchClause(matchClause);
+		query.setReturnClause(returnClause);
 	}
 	
 	@Override
@@ -39,6 +34,18 @@ public class QueryListener extends antlr4.CypherBaseListener {
 	
 	@Override
 	public void exitOC_Match(CypherParser.OC_MatchContext ctx) {
+		if (nodePattern != null) {
+			matchClause.setPattern(nodePattern);
+		}
+	}
+	
+	@Override
+	public void enterOC_NodePattern(CypherParser.OC_NodePatternContext ctx) {
+		nodePattern = new NodePattern();
+	}
+	
+	@Override
+	public void exitOC_NodePattern(CypherParser.OC_NodePatternContext ctx) {
 		
 	}
 	
