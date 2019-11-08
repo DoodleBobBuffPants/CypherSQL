f4cc3b6242ed8227fee66b8ab9c28e3c1b3923ef
DoodleBobBuffPants <ajayseanx@gmail.com>
2019-11-08 15:07:04 +0000

    Update API for current final changes

--- Translator/src/FrontEndAPI/API.java
+++ Translator/src/FrontEndAPI/API.java
@@ -3,10 +3,12 @@ package FrontEndAPI;
 import java.io.File;
 import java.nio.file.Paths;
 
+import ResultFormatter.Formatter;
+
 public class API {
 	public static void main(String[] args) {
-		if (args.length != 2) {
-			System.out.println("Must contain two arguments: <option> <argument>");
+		if (args.length < 2) {
+			System.out.println("Must contain two arguments: <option> <arguments>");
 			System.out.println("Options are --graphdb --schema --query");
 		} else {
 			String option = args[0];
@@ -20,8 +22,13 @@ public class API {
 					handleSchema(argument);
 					break;
 				case "--query":
-					handleQuery(argument);
-					break;
+					if (args.length != 3) {
+						System.out.println("Must supply 2 arguments <database name>  <query>");
+						break;
+					} else {
+						handleQuery(argument, args[2]);
+						break;
+					}
 				default:
 					System.out.println("Invalid option");
 			}
@@ -47,7 +54,7 @@ public class API {
 		System.out.println("Schema translation complete");
 	}
 	
-	private static void handleQuery(String query) {
+	private static void handleQuery(String database, String query) {
 		QueryTranslator.Translator queryTranslator;
 		if (new File(query).exists()) {
 			queryTranslator = new QueryTranslator.Translator(Paths.get(query));
@@ -57,5 +64,10 @@ public class API {
 		System.out.println("Query translation starting...");
 		System.out.println(queryTranslator.translate());
 		System.out.println("Query translation completed");
+		
+		Formatter resultFormatter = new Formatter();
+		resultFormatter.getPostgresResult(database, queryTranslator.getTranslatedQuery());
+		System.out.println("Postgres results:");
+		resultFormatter.printPostgresResult();
 	}
 }
