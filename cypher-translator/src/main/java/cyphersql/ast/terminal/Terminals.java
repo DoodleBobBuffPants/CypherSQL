71ebbb5dbfb8814a19319efd92d4fcf86f7379f8
DoodleBobBuffPants <DoodleBobBuffPants@users.noreply.github.com>
2022-11-25 23:19:14 +0000

    Apply automatic changes

--- cypher-translator/src/main/java/cyphersql/ast/terminal/Terminals.java
+++ cypher-translator/src/main/java/cyphersql/ast/terminal/Terminals.java
@@ -3,9 +3,7 @@ package cyphersql.ast.terminal;
 import cyphersql.ast.value.Value;
 
 public enum Terminals implements Terminal {
-    ENTER_LIST, EXIT_LIST,
-    ENTER_NODE_LABELS, EXIT_NODE_LABELS,
-    ENTER_MAP, EXIT_MAP;
+    ENTER_LIST, EXIT_LIST, ENTER_NODE_LABELS, EXIT_NODE_LABELS, ENTER_MAP, EXIT_MAP;
 
     @Override
     public Value<?> getValue() {
